2bdc9511ad337e24b01e833de144a06c1312fde0
Ayush Singhal <ayushsinghal2912@gmail.com>
2022-12-04 02:27:43 +0530

    server running on port 5000

---
+++ index.js
@@ -0,0 +1,9 @@
+//jshint esversion:6
+
+const express = require("express");
+const app = express();
+app.use(express.json({extended: false}));
+const PORT = 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`)); 
+
